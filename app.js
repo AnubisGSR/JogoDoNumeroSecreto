a31a11d290754d449f1a46548d8c14641674992b
AtomicX <101367631+AnubisGSR@users.noreply.github.com>
2024-08-15 22:05:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1', 'Jogo do numero do secreto');
+    exibirTextoNaTela('h1', 'Jogo do numero secreto');
     exibirTextoNaTela('p', `Escolha um numero de 1 a ${numeroLimite}`);
 }
 
